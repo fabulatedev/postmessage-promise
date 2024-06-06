224c7aa085046d2b6748da175216e40d17fc284a
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-06-05 17:00:18 -0700

    remove esm

--- esm/util.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function isWindow(el: any): boolean;
-export declare function isWindowOrIframe(el: any): boolean;
-export declare function addMessageBusToElementIfNotPresent(el: any): any;
-export type Endpoint = 'parent' | 'child';
